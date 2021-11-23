56a001976df11db9cd18248eb18f656b8aa79c88
euichol <eui-chol.shin@students.mq.edu.au>
2021-11-23 19:19:19 +1100

    Camera Implementation complete
    fully implemented camera
    now pivots left and right depending on direction car is travelling

--- COMP2160 Assignment 2/Assets/Scripts/CameraTracking.cs
+++ COMP2160 Assignment 2/Assets/Scripts/CameraTracking.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class CameraTracking : MonoBehaviour
 {
     // Specifies car to track
-    public CarController car;
+
+    public Transform target;
     public float tempX;
     public float tempY;
     public float tempZ;
@@ -13,31 +14,20 @@ public class CameraTracking : MonoBehaviour
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-//        this.transform.position = car.gameObject.transform.position;
-//        this.transform.rotation = car.gameObject.transform.rotation;
-    }
-
-    // Update is called once per frame
     void FixedUpdate()
     {
-        //        this.transform.position = car.gameObject.transform.position;
-        //        this.transform.rotation = car.gameObject.transform.rotation;
 
-        Vector3 desiredPosition = car.gameObject.transform.position + offset;
+        Vector3 desiredPosition = target.transform.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition;
 
-
-//        this.transform.position = car.gameObject.transform.position;
-
-        tempX = car.gameObject.transform.eulerAngles.x;
-        tempY = car.gameObject.transform.eulerAngles.y;
-        tempZ = car.gameObject.transform.eulerAngles.z;
+        tempX = target.transform.eulerAngles.x;
+        tempY = target.transform.eulerAngles.y;
+        tempZ = target.transform.eulerAngles.z;
 
         transform.eulerAngles = new Vector3(tempX - tempX, tempY, tempZ - tempZ);
+
+       // transform.LookAt(target);
 
     }
 }
